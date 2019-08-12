efe9a9e002445c6e1b0d347b0f65b42d31186f5e
Silas Pinho Ladislau <splhead@gmail.com>
2019-08-12 13:35:49 -0400

    continue

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Main.css";
 
-export default function Main() {
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import api from "../services/api";
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    console.log("like", id);
+  }
+
+  async function handleDislike(id) {
+    console.log("dislike", id);
+  }
+
   return (
-    <h1>Hello world</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name} />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+
+            <div className="buttons">
+              <button type="button" onClick={handleDislike}>
+                <img src={dislike} alt="Dislike" />
+              </button>
+              <button type="button" onClick={handleLike}>
+                <img src={like} alt="Like" />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
